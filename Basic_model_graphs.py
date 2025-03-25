5d7bb6b31dd3dd8935651032881c8b5aec94ff87
Lilly Abeln <lillyoleole@gmail.com>
2025-03-25 22:38:45 +0100

    New files for producing theoretical plots for the report theory

---
+++ Basic_model_graphs.py
@@ -0,0 +1,170 @@
+from typing import List, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
+from neuron import NeuronFactory, Neuron  # dataclass used for neuron parameters.
+from simulation.models.LIF_model_basic_theory import (
+    LIF_Model_basic_theory as LIF_basic,
+)  # class handling LIF models.
+from descending_drive import cortical_input  # script for creating current input.
+import seaborn as sns
+
+""" Used to plot theoretical output for diagram. """
+
+T = 1000  # Simulation Time [ms]
+DT = 0.1  # Time step in [ms]
+
+
+def Output_plots(CI: np.array, simulation_data: List[Tuple[Neuron,]]):
+    """Produces a membrane potential - Time plot for the given simulation results."""
+
+    time = np.linspace(0, np.shape(CI)[0] * DT, np.shape(CI)[0])
+
+    fig, ax = plt.subplots()
+    for neuron_data_pair in simulation_data:
+        v, sp = neuron_data_pair[1]
+        if sp.size:
+            sp_num = (sp / DT).astype(int) - 1
+            # v[sp_num] += 20  # draw nicer spikes
+
+        ax.plot(time, v, "-r")
+
+    ax.set_ylim([-80, 5])
+    ax.set_xlim([0, 200])
+    ax.axhline(
+        simulation_data[0][0].V_th_mV, color="k", ls="--"
+    )  # gets the first neurons threshold level
+    ax.set_ylabel("Membrane Potential (mV)")
+    ax.set_xlabel("Time (ms)")
+    ax.set_title("Spike Train With 1 doublet")
+    # ax.legend([neuron_data_pair[0].number for neuron_data_pair in simulation_data])
+
+
+def Freq_inst_plot(CI: np.array, simulation_data: List[Tuple[Neuron,]]):
+
+    freq = {}  # record freq over time for each neuron
+    fig, ax = plt.subplots()
+    for neuron_data_pair in simulation_data:
+        v, sp = neuron_data_pair[1]
+
+        if len(sp) < 2:
+            freq = 0  # Not enough spikes to compute frequency
+        else:
+            isi = np.diff(sp * 1e-3)  # Compute interspike intervals, time in ms
+
+            # Assign frequency values to corresponding spike times (excluding the first spike)
+            freq[neuron_data_pair[0].number] = np.concatenate(
+                ([0], 1 / isi)
+            )  # First spike has no frequency
+
+            ax.plot(sp, freq[neuron_data_pair[0].number], ".")
+
+    ax.set_ylabel("Frequency (HZ)")
+    ax.set_xlabel("Time (ms)")
+    ax.set_title("Frequency-time Plot")
+    ax.legend([neuron_data_pair[0].number for neuron_data_pair in simulation_data])
+
+
+def output_heatmat(CI: np.array, simulation_data: List[Tuple[Neuron,]]):
+
+    time = np.linspace(0, np.shape(CI)[0] * DT, np.shape(CI)[0])
+    outputs = []
+    for neuron_data_pair in simulation_data:
+        v, sp = neuron_data_pair[1]
+        if sp.size:
+            sp_num = (sp / DT).astype(int) - 1
+            v[sp_num] += 20  # draw nicer spikes
+        outputs.append(v)
+
+    plt.figure(figsize=(12, 6))
+    sns.heatmap(
+        outputs,
+        cmap="coolwarm",
+        xticklabels=1000,
+        yticklabels=[
+            neuron_data_pair[0].number for neuron_data_pair in simulation_data
+        ],
+    )
+
+    plt.xlabel("Time (steps)")
+    plt.ylabel("Neurons")
+    plt.title("Neuron Output Heatmap")
+
+
+def _main():
+
+    ## ------------- Simulation Parameters ------------- ##
+
+    # T = 1000  # Simulation Time [ms]
+    # DT = 0.1  # Time step in [ms]
+    neuron_pool_size = 300  # Total number of Neurons in the pool
+
+    ## SELECT THE MODEL TO RUN
+
+    model_choice = LIF_basic  # Options: LIF_Model1, LIF_Model2
+
+    ## -- Cortical input - simulation parameters -- ##
+
+    number_of_clusters = 5  # Number of clusters
+    max_I = 40  # Max input current (nA)
+    CCoV = 0  # Cluster-common noise CoV (%)
+    ICoV = 0  # Independent noise CoV (%)
+    signal_type = "trapezoid"  # Options:  "sinusoid.hz" -- "trapezoid" -- "triangular" -- "step-sinusoid" -- "step"
+    freq = 2  # Frequency for sinusoid
+
+    CI = cortical_input(
+        neuron_pool_size,
+        number_of_clusters,
+        max_I,
+        T,
+        DT,
+        CCoV,
+        ICoV,
+        signal_type,
+        freq,
+    )
+
+    input = max_I * np.ones(int(T / DT))
+    # Create neurons with NEURONFACTORY class (# Generate list of new neurons)
+
+    all_neurons = NeuronFactory.create_neuron_pool(number_of_neurons=neuron_pool_size)
+    neuron_indexes = [50, 100, 250]  # Neurons to be modelled & plotted.
+    neuron_indexes = [
+        5,
+        10,
+        50,
+        100,
+        120,
+        200,
+        230,
+        250,
+        270,
+        280,
+        290,
+    ]  # Neurons to be modelled & plotted.
+
+    # neurons_to_simulate = [all_neurons[i] for i in neuron_indexes]
+    # neurons_to_simulate = all_neurons # OPTION: Use this to model all 300 neurons!
+    neurons_to_simulate = [all_neurons[200]]
+
+    ##-------------------------------------------------- ##
+
+    # Create a list of (neuron-object , (v, sp) ) data-pairs
+    simulation_results = [
+        (
+            b,
+            model_choice.simulate_neuron(T, DT, neuron=b, Iinj=input),
+        )
+        for b in neurons_to_simulate
+    ]
+
+    # TODO Some Plotting choices
+
+    Output_plots(CI, simulation_results)
+    # Freq_inst_plot(CI, simulation_results)
+    # output_heatmat(CI, simulation_results)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    _main()
---
+++ simulation/models/LIF_model_basic_theory.py
@@ -0,0 +1,76 @@
+import numpy as np
+from neuron import Neuron
+from typing import Tuple
+from simulation.simulate import TimestepSimulation
+
+##BASIC LIF MODEL - Used to create output plot for thesis, not accurate modelling##
+
+
+class LIF_Model_basic_theory(TimestepSimulation):
+
+    @staticmethod
+    def simulate_neuron(
+        sim_time: np.float64, timestep: np.float64, neuron: Neuron, Iinj: np.array
+    ) -> Tuple[np.array, np.array]:
+        """
+        Simulate the LIF dynamics with external input current
+
+        Args:
+        neuron       : Neuron object containing parameters
+        Iinj       : input current [nA]. The injected current here can be a value
+                    or an array
+
+        Returns:
+        rec_v      : membrane potential
+        rec_sp     : spike times
+        """
+
+        simulation_steps = len(np.arange(0, sim_time, timestep))
+
+        # Initialize voltage
+        v = np.zeros(simulation_steps)
+        v[0] = neuron.V_init_mV
+
+        # Set current time course
+        # Iinj = Iinj * np.ones(sim_steps)
+
+        # Loop over time
+        rec_spikes = []  # record spike times
+        tr = 0.0  # the count for refractory duration
+
+        for it in range(simulation_steps - 1):
+
+            if tr > 0:  # check if in refractory period
+                # Gradually decay voltage toward reset using exponential decay
+                tau_refractory = 5  # you can tune this time constant (in time steps)
+                v[it] = v[it - 1] + (neuron.V_reset_mV - v[it - 1]) / tau_refractory
+                tr -= 1  # decrement refractory counter
+                if len(rec_spikes) < 2:
+                    if tr < 0:  # increased excitation for doublet.
+                        v[it] = neuron.V_reset_mV / 2
+
+            elif v[it] >= neuron.V_th_mV:  # if voltage over threshold
+                rec_spikes.append(it)  # record spike event
+                if len(rec_spikes) == 2:
+
+                    v[it] = -10  # spike voltage for doublet
+                    tr = neuron.tref / timestep * 2
+                else:
+                    v[it] = 0  # spike voltage
+                    # v[it] = neuron.V_reset_mV  # reset voltage
+                    tr = neuron.tref / timestep  # set refractory time
+
+            # Calculate the increment of the membrane potential
+            dv = (
+                -(neuron.gain_leak) * (v[it] - neuron.E_L_mV)
+                + (neuron.gain_exc) * (Iinj[it] * neuron.R_Mohm)
+            ) * (timestep / neuron.tau_ms)
+
+            # Update the membrane potential [mv]
+            v[it + 1] = v[it] + dv
+
+        # Get spike times in ms
+        rec_spikes = np.array(rec_spikes) * timestep
+        # print(doub_count)
+
+        return v, rec_spikes
