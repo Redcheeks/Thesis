b7806da9e9e5aec79e9ae4e9399f512eb2010b05
Lilly Abeln <lillyoleole@gmail.com>
2025-04-05 17:32:47 +0200

    Reworking neuron model. new input to choose between distribution for rheobase or calculation.

--- neuron.py
+++ neuron.py
@@ -8,8 +8,8 @@ V_RESET: np.float64 = -75.0  # Hyperpolarization, membrane potential in mV
 V_INIT: np.float64 = -70.0  # Initial potential in mV
 E_LEAK: np.float64 = -70.0  # leak reversal potential in mV
 
-TREF_FRAC_AHP: np.float64 = 0.025  # t_ref as a fraction of AHP.
-SOMA_DIAM_MIN_METER: np.float64 = 40e-6
+TREF_FRAC_AHP: np.float64 = 0.2  # t_ref as a fraction of AHP.
+SOMA_DIAM_MIN_METER: np.float64 = 50e-6
 SOMA_DIAM_MAX_METER: np.float64 = 100e-6
 
 # From francois github :
@@ -21,98 +21,43 @@ SOMA_DIAM_MAX_METER: np.float64 = 100e-6
 
 @dataclass(frozen=True)
 class Neuron:
+    # __________Fixed Variables___________#
+    distribution: bool
+    """True - use distribution rheobase values"""
+
     number: int
     """MN number/index"""
+
     D_soma_meter: np.float64  # used for calculations, not for model
     """Soma Diameter [m]"""
+
     V_rest_mV: np.float64
     """Resting potential [mV]"""
+
     V_th_mV: np.float64
     V_reset_mV: np.float64
     V_init_mV: np.float64
     E_L_mV: np.float64
-    # TODO Write in thesis how unrealistic these arbitrary linear distributions are.
-    doublet_rheobase_coefficient: (
-        np.float64
-    )  # Rheobase coefficient for doublet threshold
-    num_total_neurons: np.float64
 
+    # TODO Write in thesis how unrealistic these arbitrary linear distributions are.
+    doublet_rheobase_coefficient: np.float64
+    """Rheobase coefficient for doublet threshold"""
+
+    num_total_neurons: np.float64
     gain_leak: np.float64
     gain_exc: np.float64
 
+    # __________Calculated Variables___________#
     @property
     def D_soma(self) -> np.float64:
         """Soma diameter in [μm]"""
         return self.D_soma_meter * 1e6  # Soma diameter in [μm]
 
-    @property
-    def S_soma_meter(self) -> np.float64:
-        """Soma Surface Area in [m^2]"""  # From Caillet table 4
-        return self.D_soma_meter * 5.5e-3  # square meters [m^2].
-
-    @property
-    def S_soma(self) -> np.float64:
-        """Soma Surface Area in micro_Meters ^2"""
-        return (
-            self.S_soma_meter * 1e12  # mAtHEmAtiCs need to square the micro also
-        )  # Neuron surface area  [μ m^2]
-
-    @property
-    def R_Mohm(self) -> np.float64:
-        """Membrane Resistance in Mega_Ohm"""  # From Caillet table 4
-        R_unit = 1.68e-10 * np.pow(self.S_soma_meter, -2.43)  # Input resistance [Ω]
-        return R_unit * 1e-6  # Input resistance [MΩ]
-
-    @property
-    def C_nF(self) -> np.float64:
-        """Membrane capacitance nano_Farad"""  # From Caillet table 4
-        C_unit = 7.9e-5 * self.D_soma_meter  # Membrane capacitance [F]
-        return C_unit * 1e9  # Membrane capacitance [nF]
-
-    @property
-    def tau_ms(self) -> np.float64:
-        """Membrane time constant milli_Seconds"""  # From Caillet table 4
-        tau_unit = 2.3e-9 * np.pow(
-            self.D_soma_meter, -1.48
-        )  # 7.9e-5 * D_soma_unit * R_unit  # Membrane time constant [s]
-        return tau_unit * 1e3  # Membrane time constant [ms]
-
-    # @property  # OPTION : from Caillet code - Rheobase Distribution
-    # def I_rheo_distr_ampere(self) -> np.float64:
-    #     """Rheobase current distribution Ampere"""
-    #     return 3.85e-9 * np.pow(
-    #         9.1, np.pow((self.number / self.num_total_neurons), 1.1831)
-    #     )
-    #     # Rheobase current [A]
-
     # @property
-    # def I_rheo_distr(self) -> np.float64:
-    #     """Rheobase current distribution nanoAmpere"""
-    #     return self.I_rheo_distr_ampere * 1e9  # Rheobase current [nA]
-
-    # @property
-    # def R_I_Mohm(self) -> np.float64:
-    #     """Membrane Resistance from Rheobase Current in Mega_Ohm"""  # From Caillet table 4
-    #     R_unit = 3.1e-2 * np.pow(
-    #         self.I_rheo_distr_ampere, -0.96
-    #     )  # Input resistance [Ω]
-    #     return R_unit * 1e-6  # Input resistance [MΩ]
-
-    @property  # ALT OPTION: From Caillet table 4
-    def I_rheo_ampere(self) -> np.float64:
-        return 7.8e2 * np.pow(self.D_soma_meter, 2.52)  # Rheobase current [A]
-
-    @property
-    def I_rheobase(self) -> np.float64:
-        """Rheobase current nano_Ampere"""
-        return self.I_rheo_ampere * 1e9  # Rheobase current [nA]
-
-    @property
-    def Rheobase_threshold(self) -> np.float64:
-        """Doublet threshold (Rheobase current) nano_Ampere"""  # From Paper https://doi.org/10.1007/s00221-010-2339-7 "Doublet of action potentials evoked by intracellular injection of rectangular depolarization current into rat motoneurones"
-        return (
-            self.I_rheobase * self.doublet_rheobase_coefficient
-        )  # Doublet current threshold [nA]
+    # def C_nF(self) -> np.float64:
+    #     """Membrane capacitance nano_Farad"""  # From Caillet table 4
+    #     C_unit = 7.9e-5 * self.D_soma_meter  # Membrane capacitance [F]
+    #     return C_unit * 1e9  # Membrane capacitance [nF]
 
     @property
     def AHP_seconds(self) -> np.float64:  # From Caillet table 4
@@ -131,11 +76,79 @@ class Neuron:
         return self.tref_seconds * 1e3  # Refractory time [ms]
 
     @property
-    def g_L(self) -> np.float64:
-        """Leak conductance  micro_Siemens"""
-        # (g_L = C / tau or 1 / R)
-        return 1 / self.R_Mohm * 10  # Leak Conductance [μS]
+    def I_rheobase_ampere(self) -> np.float64:
+        """Rheobase current Ampere"""
+        if self.distribution:
+            ### --- OPTION 1 : from Caillet code - Rheobase Distribution
+            return 3.85e-9 * np.pow(
+                9.1, np.pow((self.number / self.num_total_neurons), 1.1831)
+            )
+            # Rheobase current [A]
+        else:
+            ### --- OPTION 2: From Caillet table 4
+            return 7.8e2 * np.pow(self.D_soma_meter, 2.52)  # Rheobase current [A]
 
+    @property
+    def I_rheobase(self) -> np.float64:
+        """Rheobase current nanoAmpere"""
+        return self.I_rheobase_ampere * 1e9  # Rheobase current [nA]
+
+    @property
+    def R_Mohm(self) -> np.float64:
+        """Membrane Resistance in Mega_Ohm"""  # From Caillet table 4
+        if self.distribution:
+            R_unit = 3.1e-2 * np.pow(
+                self.I_rheobase_ampere, -0.96
+            )  # Input resistance [Ω]
+        else:
+            R_unit = 1.68e-10 * np.pow(
+                self.D_soma_meter * 5.5e-3, -2.43
+            )  # Input resistance [Ω]
+        return R_unit * 1e-6  # Input resistance [MΩ]
+
+    @property
+    def tau_ms(self) -> np.float64:
+        """Membrane time constant milli_Seconds"""
+        # tau_unit = 2.3e-9 * np.pow(self.D_soma_meter, -1.48) # From Caillet table 4
+        tau_unit = (
+            7.9e-5 * self.D_soma_meter * (self.R_Mohm * 1e6)
+        )  # Membrane time constant [s]
+        return tau_unit * 1e3  # Membrane time constant [ms]
+
+    # @property
+    # def S_soma_meter(self) -> np.float64:
+    #     """Soma Surface Area in [m^2]"""  # From Caillet table 4
+    #     return self.D_soma_meter * 5.5e-3  # square meters [m^2].
+
+    # @property
+    # def S_soma(self) -> np.float64:
+    #     """Soma Surface Area in micro_Meters ^2"""
+    #     return (
+    #         self.S_soma_meter * 1e12  # mAtHEmAtiCs need to square the micro also
+    #     )  # Neuron surface area  [μ m^2]
+
+    # @property
+    # def R_Mohm(self) -> np.float64:
+    #     """Membrane Resistance in Mega_Ohm"""  # From Caillet table 4
+    #     R_unit = 1.68e-10 * np.pow(
+    #         self.D_soma_meter * 5.5e-3, -2.43
+    #     )  # Input resistance [Ω]
+    #     return R_unit * 1e-6  # Input resistance [MΩ]
+
+    @property
+    def Rheobase_threshold(self) -> np.float64:
+        """Doublet threshold (Rheobase current) nano_Ampere"""  # From Paper https://doi.org/10.1007/s00221-010-2339-7 "Doublet of action potentials evoked by intracellular injection of rectangular depolarization current into rat motoneurones"
+        return (
+            self.I_rheobase * self.doublet_rheobase_coefficient
+        )  # Doublet current threshold [nA]
+
+    # @property
+    # def g_L(self) -> np.float64:
+    #     """Leak conductance  micro_Siemens"""
+    #     # (g_L = C / tau or 1 / R)
+    #     return 1 / self.R_Mohm * 10  # Leak Conductance [μS]
+
+    # _______ Helper Functions ________#
     def calculate_v_reset(self, Iinj_it):
         """
         Calculates a linearly distributed reset voltage based on the injected current.
@@ -160,6 +173,7 @@ class NeuronFactory:
 
     @staticmethod
     def create_neuron(
+        distribution: bool,
         soma_diameter: np.float64,
         number: int,
         num_total_neurons: int,  # Needed for rheobase threshold coefficients
@@ -169,6 +183,7 @@ class NeuronFactory:
             number
         ]  # Leakage coefficient
         return Neuron(
+            distribution=distribution,
             number=number,
             doublet_rheobase_coefficient=np.linspace(3.4, 2.1, num_total_neurons)[
                 number
@@ -185,13 +200,16 @@ class NeuronFactory:
         )
 
     @staticmethod
-    def create_neuron_pool(number_of_neurons: np.float64 = 300) -> List[Neuron]:
+    def create_neuron_pool(
+        distribution: bool, number_of_neurons: np.float64 = 300
+    ) -> List[Neuron]:
         """Create a neuron pool for a number of neurons"""
 
         soma_diameters = soma_diameter_vector(total_neurons=number_of_neurons)
 
         neuron_list = [
             NeuronFactory.create_neuron(
+                distribution=distribution,
                 soma_diameter=soma_diameter,
                 number=i,
                 num_total_neurons=number_of_neurons,
