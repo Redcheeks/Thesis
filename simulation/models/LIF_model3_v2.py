4c893955cb8fb633ead3b21a71473be747732ff8
Lilly Abeln <lillyoleole@gmail.com>
2025-04-23 18:30:14 +0200

    New Model3 file for redoing underlying logic.

---
+++ simulation/models/LIF_model3_v2.py
@@ -0,0 +1,107 @@
+import numpy as np
+from neuron import Neuron
+from typing import Tuple
+from simulation.simulate import TimestepSimulation
+
+##MODEL WITH VARIABLE RESET VOLTAGE AND post-doublet-depression##
+
+
+class LIF_Model3v2(TimestepSimulation):
+
+    @staticmethod
+    def simulate_neuron(
+        sim_time: np.float64, timestep: np.float64, neuron: Neuron, Iinj: np.array
+    ) -> Tuple[np.array, np.array, np.array, np.array]:
+        """
+        Simulate the LIF dynamics with external input current
+
+        Args:
+        neuron       : Neuron object containing parameters
+        Iinj       : input current [nA]. The injected current here can be a value
+                    or an array
+
+        Returns:
+        rec_v      : membrane potential
+        rec_sp     : spike times
+        inhib_trace: inhibition decay factor over time
+        reset_trace: reset voltage trace over time
+        """
+
+        simulation_steps = len(np.arange(0, sim_time, timestep))
+
+        # Initialize voltage
+        v = np.zeros(simulation_steps)
+        v[0] = neuron.V_init_mV
+        V_reset_it = neuron.V_reset_mV
+
+        inhib_trace = np.zeros(simulation_steps)
+        reset_trace = np.full(simulation_steps, np.nan)
+
+        # Set current time course
+        # Iinj = Iinj * np.ones(sim_steps)
+
+        # Loop over time
+        rec_spikes = []  # record spike times
+        tr = 0.0  # the count for refractory duration
+        last_spike_counter = (
+            100 / timestep
+        )  # time since spike, used for doublet interval (3-10ms).
+
+        doublet_block = 0.0
+
+        for it in range(simulation_steps - 1):
+
+            if tr > 0:  # check if in refractory period
+                # TODO: could be a nice curve down rather than a steep drop
+                v[it] = V_reset_it  # set voltage to reset
+                reset_trace[it] = V_reset_it
+
+                tr = tr - 1  # reduce running counter of refractory period
+
+            elif v[it] >= neuron.V_th_mV:
+                ## ---- DOUBLET ---- ##
+                if (
+                    last_spike_counter < 10 / timestep
+                    and Iinj[it] >= neuron.I_rheobase
+                    and doublet_block < 0.5
+                ):
+                    v[it] = 18  # 18mV for doublet
+                    rec_spikes.append(it)
+                    last_spike_counter = 0.0
+                    doublet_block = 1.0
+                    V_reset_it = neuron.calculate_v_reset_MODEL3v2(
+                        Iinj[it], doublet_block
+                    )
+                    tr = neuron.tref * 2 / timestep
+                ## ---- NORMAL SPIKE ---- ##
+                else:
+                    rec_spikes.append(it)
+
+                    v[it] = 20  # 20mV biologically accurate?
+                    V_reset_it = neuron.calculate_v_reset(Iinj[it])
+                    tr = neuron.tref / timestep
+                    last_spike_counter = 0.0
+            reset_trace[it] = V_reset_it
+            # Calculate the increment of the membrane potential
+            dv = (
+                -(neuron.gain_leak) * (v[it] - neuron.E_L_mV)
+                + (neuron.gain_exc) * (Iinj[it] * neuron.R_Mohm)
+            ) * (timestep / neuron.tau_ms)
+
+            # Update the membrane potential [mv]
+            v[it + 1] = v[it] + dv
+            last_spike_counter += 1
+
+            doublet_block *= np.exp(-timestep / 500.0)
+
+            inhib_trace[it] = doublet_block
+
+        # for i in range(1, simulation_steps):
+        #     if np.isnan(reset_trace[i]):
+        #         reset_trace[i] = reset_trace[i - 1]
+
+        # Get spike times in ms
+        rec_spikes = np.array(rec_spikes) * timestep
+        # print(doub_count)
+
+        return v, rec_spikes, inhib_trace, reset_trace
