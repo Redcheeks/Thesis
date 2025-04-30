8988737ab2e6690f429c470092f40e7d40098456
Lilly Abeln <lillyoleole@gmail.com>
2025-04-30 14:18:59 +0200

    Pick which data is being plotted

--- Experimental Data Analysis/Experimental_analysis.py
+++ Experimental Data Analysis/Experimental_analysis.py
@@ -11,19 +11,22 @@ def _main():
 
     trapezoid = scipy.io.loadmat("Experimental Data Analysis/trapezoid20mvc.mat")
 
-    data_to_plot = trapezoid
+    data_to_plot = trapezoid_sinusoid2hz
     plt.ion()  # Turn on interactive mode
 
-    for ind in np.arange(data_to_plot["discharge_times"][0].shape[0]):
+    for ind in np.arange(
+        data_to_plot["discharge_times"][0].shape[0]
+    ):  # for each neuron
         plt.close()
         fig, ax = plt.subplots()
+        # plot , frequency.
         ax.plot(
             data_to_plot["discharge_times"][0][ind][0][:-1] / 2e3,
             1000 / (np.diff(data_to_plot["discharge_times"][0][ind][0][:]) / 2),
             "o",
         )
         ax.set_ylabel("Instantaneous Frequency [Hz]")
-        ax.set_xlabel("Time [ms]")
+        ax.set_xlabel("Time [s]")
         ax.set_title("Neuron number" + str(ind))
         plt.show()
         # plt.pause()
